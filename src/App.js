a0f7f415f109fc0f5f0aadba2f1d9a63759388cb
Ali <alihassnain330@gmail.com>
2023-09-29 01:02:57 +0500

    feature of start capturig and stop caturing add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+//import Webcam from "react-webcam";
+import WebcamImage from "./WebcamImg";
+
+function App() {
+  return (
+    <div className="App">
+      <WebcamImage />
+    </div>
+  );
+}
+
+export default App;
